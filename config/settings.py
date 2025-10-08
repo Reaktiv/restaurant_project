54a932200b2a8d15d44d0b90822265da390f2a5c
Reaktiv <itolmasov.olmasjon1505@gmail.com>
2025-10-08 08:59:09 +0500

    Updated settings for MySQL and dotenv configuration

--- config/settings.py
+++ config/settings.py
@@ -14,17 +14,19 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p7&@-o&tga#)r$8fq2rz))yn4gyj54!_b!px!i@)yo2ujxh@__'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
