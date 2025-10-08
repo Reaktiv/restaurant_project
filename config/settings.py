a51da2266128b2f6fc0d1fe7cce5361ee9a75ead
Reaktiv <itolmasov.olmasjon1505@gmail.com>
2025-10-08 14:06:45 +0500

    local updates

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
